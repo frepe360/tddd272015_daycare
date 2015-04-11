01048af8ea325bfc0a4f1efbbec0ea30517e4942
Fredrik Persson <frepe360@student.liu.se>
2015-04-11 17:25:36 +0200

    Fix IP and PORT no for openshift

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express();
 
+var serverip = process.env.OPENSHIFT_NODEJS_IP;
+var serverport = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/', function(req, res) {
     res.send('Hello world');
 })
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(serverport, serverip, function() {
+    console.log('%s: Node server started on %s:%d ...', Date(Date.now()), serverip, serverport);
+});
+
