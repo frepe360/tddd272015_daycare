ea7b254cd69a2bc269dbe105250eee60feb1e163
Fredrik Persson <frepe360@student.liu.se>
2015-04-12 15:30:26 +0200

    Add static route to public folder, in order for front end to be able to fetch scripts

--- serverconfig.js
+++ serverconfig.js
@@ -6,6 +6,8 @@ var app = express();
 app.set('views', path.normalize(__dirname + '/server/views'));
 app.set('view engine', 'jade');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('*', function(req, res) {
    res.render('index');
 });
