06ddcd8ff9b059ac144348aa69c768d032535e32
Fredrik Persson <frepe360@student.liu.se>
2015-04-12 12:11:11 +0200

    Configure express to use jade as the view engine, and render the index page from the index view

--- serverconfig.js
+++ serverconfig.js
@@ -1,10 +1,13 @@
+var path = require('path');
 var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res) {
-    res.status(200);
-    res.end();
-})
+app.set('views', path.normalize(__dirname + '/server/views'));
+app.set('view engine', 'jade');
+
+app.get('*', function(req, res) {
+   res.render('index');
+});
 
 module.exports = app;
