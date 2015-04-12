3c9d6508008b361cc5708916153a720d0361b2af
Fredrik Persson <frepe360@student.liu.se>
2015-04-12 12:23:45 +0200

    Print the node version on startup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = require('./serverconfig');
 var serverip = process.env.OPENSHIFT_NODEJS_IP;
 var serverport = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
+console.log('My node version is: ' + process.version);
+
 app.listen(serverport, serverip, function() {
     console.log('%s: Node server started on %s:%d ...', Date(Date.now()), serverip, serverport);
 });
