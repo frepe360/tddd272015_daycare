86eeccdd7ed2e2aff461f6f4e503be6aa5ce4e53
Fredrik Persson <frepe360@student.liu.se>
2015-04-12 19:11:13 +0200

    Add to bower.json and links to use it in the index view

--- serverconfig.js
+++ serverconfig.js
@@ -6,6 +6,13 @@ var app = express();
 app.set('views', path.normalize(__dirname + '/server/views'));
 app.set('view engine', 'jade');
 
+// This is an ugly hack; node would not set the content-type to text/css
+// it this was served via the static route.
+app.get('/vendor/bootstrap/dist/bootstrap.css', function(req, res) {
+   res.type('text/css');
+   res.sendFile(__dirname + '/public/vendor/bootstrap/dist/css/bootstrap.css');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
