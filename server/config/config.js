451c9a173aae5a0e669e0a6bb8adf6cdb76c7963
Fredrik Persson <frepe360@student.liu.se>
2015-04-16 20:31:50 +0200

    Add server IP for openshift

--- server/config/config.js
+++ server/config/config.js
@@ -5,11 +5,13 @@ module.exports = {
     development: {
         db: 'mongodb://db_user:db_password@localhost/daycaredb',
         rootPath: rootPath,
-        serverport: process.env.OPENSHIFT_NODEJS_PORT || 3000
+        serverport: process.env.OPENSHIFT_NODEJS_PORT || 3000,
+        serverip: undefined
 },
     production: {
         db: 'mongodb://admin:gKP6_hS2fRv1@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT,
         rootPath: rootPath,
-        serverport: process.env.OPENSHIFT_NODEJS_PORT || 80
+        serverport: process.env.OPENSHIFT_NODEJS_PORT || 80,
+        serverip: process.env.OPENSHIFT_NODEJS_IP
     }
 };
