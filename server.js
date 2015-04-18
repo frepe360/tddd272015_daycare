7347d63bcadb75494bb51bb10fd123540aaeb2a0
Fredrik Persson <frepe360@student.liu.se>
2015-04-18 11:13:08 +0200

    Refactoring: Create config file for express, move stuff from server.js to express.js config file

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ var config = require('./server/config/config.js')[env];
 
 require('./server/config/mongoose.js')(config);
 
-app.set('views', config.rootPath + '/server/views');
-app.set('view engine', 'jade');
+require('./server/config/express.js')(app, config);
 
 // This is an ugly hack; node would not set the content-type to text/css
 // it this was served via the static route.
@@ -17,8 +16,6 @@ app.get('/vendor/bootstrap/dist/bootstrap.css', function(req, res) {
     res.sendFile(config.rootPath + '/public/vendor/bootstrap/dist/css/bootstrap.css');
 });
 // Ugly hack ends here
-
-app.use(express.static(config.rootPath + '/public'));
 
 app.get('*', function(req, res) {
     res.render('index');
