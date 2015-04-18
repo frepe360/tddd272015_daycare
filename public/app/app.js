a08b740e5b3da1244b3949177ab4208d9dc8ec18
Fredrik Persson <frepe360@student.liu.se>
2015-04-18 12:20:44 +0200

    Add angular-route to the angular app

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', []);
+angular.module('app', ['ngRoute']);
 
 angular.module('app').controller('mainCtrl', function($scope) {
     $scope.testvar = 'this is a test variable to make sure the controller works';
