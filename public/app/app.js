dc2f588d741392045e3de9490ca94a383e209c40
Fredrik Persson <frepe360@student.liu.se>
2015-04-18 16:47:18 +0200

    Add contact info on front page and a FAQ page

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,7 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', { templateUrl: 'partials/main', controller: 'mainCtrl'})
+        .when('/faq', { templateUrl: 'partials/faq', controller: 'faqCtrl'})
 });
 
 angular.module('app').controller('mainCtrl', function($scope) {
