2f9a6ad7a5877d68389dd938c987c5561c924e31
Fredrik Persson <frepe360@student.liu.se>
2015-04-18 15:48:23 +0200

    Enable html5mode for locationProvider, to make deep links work

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,7 @@
 angular.module('app', ['ngResource', 'ngRoute']);
 
-angular.module('app').config(function($routeProvider) {
+angular.module('app').config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', { templateUrl: 'partials/main', controller: 'mainCtrl'})
 });
@@ -9,3 +10,6 @@ angular.module('app').controller('mainCtrl', function($scope) {
     $scope.testvar = 'this is a test variable to make sure the controller works';
 });
 
+angular.module('app').controller('faqCtrl', function($scope) {
+    $scope.testvar = 'this is a faq variable to make sure the controller works';
+});
