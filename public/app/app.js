69d50d79d64af909983eefe22ccdfa9e317fd2ba
Fredrik Persson <frepe360@student.liu.se>
2015-04-18 12:38:51 +0200

    Add angular-resource to the angular app

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').controller('mainCtrl', function($scope) {
     $scope.testvar = 'this is a test variable to make sure the controller works';
