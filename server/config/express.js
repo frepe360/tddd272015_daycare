350c1502d775fd582d88853e82cbae192b8694f1
Fredrik Persson <frepe360@student.liu.se>
2015-04-19 17:01:52 +0200

    Small cleanup

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-module.exports = function(app, config) {
+var express = require('express'),
+    bodyParser = require('body-parser');
 
+module.exports = function(app, config) {
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
     app.use(express.static(config.rootPath + '/public'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-
 };
