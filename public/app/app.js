5f0a99ee0bf6f8c4c70f63fecba4f09f3dfc35d5
Fredrik Persson <frepe360@student.liu.se>
2015-04-19 11:13:25 +0200

    Refactoring: Rename angular controllers, the names shall be prefixed with 'dc' short for 'daycare'

--- public/app/app.js
+++ public/app/app.js
@@ -3,14 +3,14 @@ angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when('/', { templateUrl: 'partials/main', controller: 'mainCtrl'})
-        .when('/faq', { templateUrl: 'partials/faq', controller: 'faqCtrl'})
+        .when('/', { templateUrl: 'partials/main', controller: 'dcMainCtrl'})
+        .when('/faq', { templateUrl: 'partials/faq', controller: 'dcFaqCtrl'})
 });
 
-angular.module('app').controller('mainCtrl', function($scope) {
+angular.module('app').controller('dcMainCtrl', function($scope) {
     $scope.testvar = 'this is a test variable to make sure the controller works';
 });
 
-angular.module('app').controller('faqCtrl', function($scope) {
+angular.module('app').controller('dcFaqCtrl', function($scope) {
     $scope.testvar = 'this is a faq variable to make sure the controller works';
 });
