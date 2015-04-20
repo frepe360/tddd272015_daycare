2340f083e803bc7513c795dce62b1f26c4b45d97
Fredrik Persson <frepe360@student.liu.se>
2015-04-20 16:02:17 +0200

    Add a user model to the database and populate it with some test users

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,25 @@
 var mongoose = require('mongoose');
 
+var userSchema = mongoose.Schema({
+    firstName: {type: String},
+    lastName: {type: String},
+    username: {type: String},
+    password: {type: String}
+});
+
+var User = mongoose.model('User', userSchema);
+
+User.find({}).exec(function(err, collection) {
+    if(collection.length === 0) {
+        console.log('Creating test users');
+        // TODO: Disable test users for production systems
+        User.create({firstName: 'Fredrik', lastName: 'Persson', username: 'fredrik', password: 'fredrik'});
+        User.create({firstName: 'Alice', lastName: 'Persson', username: 'alice', password: 'alice'});
+    } else {
+        console.log('Test users created already');
+    }
+});
+
 module.exports = function(config) {
     mongoose.connect(config.db, function(err) {
         if(err) {
