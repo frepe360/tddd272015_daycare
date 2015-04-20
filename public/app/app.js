6e4b8fbf41d7691442c76c52ddb8615933821c96
Fredrik Persson <frepe360@student.liu.se>
2015-04-20 15:27:58 +0200

    Hide the login stuff if the user is authenticated

--- public/app/app.js
+++ public/app/app.js
@@ -16,13 +16,16 @@ angular.module('app').controller('dcFaqCtrl', function($scope) {
 });
 
 angular.module('app').controller('dcNavBarLoginCtrl', function($scope, $http) {
+    $scope.userAuthenticated = false;
     $scope.signin = function(username, password) {
         console.log('Sending POST to /login with username: ' + username + ' and password ' + password);
         $http.post('/login', {username: username, password: password}).then(function() {
             console.log('Successfully logged in!');
+            $scope.userAuthenticated = true;
         },
         function() {
             console.log('Login failed!')
+            $scope.userAuthenticated = false;
         });
     }
 });
