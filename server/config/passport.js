b0b19b441ad21a472ec23c82bd019dde3b4d3bdd
Fredrik Persson <frepe360@student.liu.se>
2015-04-20 16:24:29 +0200

    Make passport check the database for user credentials instead of using hardcoded test values

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,11 +1,17 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    mongoose = require('mongoose');
 
 module.exports = function() {
+
     passport.use(new LocalStrategy(function(username, password, done) {
-        if (username === 'asdf' && password === 'asdf') {
-            return done(null, true);
-        }
-        return done(null, false);
+        var User = mongoose.model('User');
+        User.findOne({username: username}).exec(function(err, user) {
+            if (user && user.password === password) {
+                return done(null, true);
+            }
+            return done(null, false);
+        })
     }));
+
 };
